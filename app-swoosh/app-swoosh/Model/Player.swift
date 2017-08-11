283dda25cc6a83975a028293517768912b9c9658
panmajki <michal@Pro-panmajki.local>
2017-08-11 14:00:03 +0200

    IBActions handling events

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by panmajki on 11.08.2017.
+//  Copyright © 2017 panmajki. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
